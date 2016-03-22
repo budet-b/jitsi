1a25a1ad3b2d1485eb56e7e0c73d4faf8e9c7cde
sacha brami <sacha.brami@epitech.eu>
2016-03-22 16:25:30 -0700

    add tbi interaction for jitsi

---
+++ jitsi-meet/angular-scripts/EasyAcademy-CV.js
@@ -0,0 +1,17 @@
+var app = angular.module("EasyAcademyCV", []);
+
+app.controller("ClasseVirtuelle", function ($scope) {
+	$scope.enableWhiteBoard = false;
+
+	$scope.activateWhiteBoard = function() {
+		if ($scope.enableWhiteBoard) {
+			$scope.enableWhiteBoard = false;
+			$("#toolbar_button_whiteboard").removeClass('active');
+		}
+		else if (!$scope.enableWhiteBoard) {
+			$scope.enableWhiteBoard = true;
+			$("#toolbar_button_whiteboard").addClass('active');
+			APP.conference.toggleScreenSharing();
+		}
+	}
+});
